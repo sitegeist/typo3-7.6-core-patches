fe40729e375fa1d0fe5c8bce2b88ce4abe73931b
Andreas Wolf <dev@a-w.io>
2016-07-05 19:49:48 +0200

    [BUGFIX] Make bidirectional reference count compatible with legacy code
    Related: #22099

--- typo3/sysext/recordlist/Classes/RecordList/DatabaseRecordList.php
+++ typo3/sysext/recordlist/Classes/RecordList/DatabaseRecordList.php
@@ -1845,7 +1845,7 @@ class DatabaseRecordList extends AbstractDatabaseRecordList
             'sys_refindex',
             'tablename=' . $this->getDatabaseConnection()->fullQuoteStr($tableName, 'sys_refindex') . ' AND recuid=' . (int)$uid
         );
-        return $this->generateReferenceToolTip($referenceCount, $referenceFromCount, GeneralUtility::quoteJSvalue($tableName) . ', ' . GeneralUtility::quoteJSvalue($uid));
+        return $this->generateBidirectionalReferenceToolTip($referenceCount, $referenceFromCount, GeneralUtility::quoteJSvalue($tableName) . ', ' . GeneralUtility::quoteJSvalue($uid));
     }
 
     /**
@@ -1857,7 +1857,7 @@ class DatabaseRecordList extends AbstractDatabaseRecordList
      * @param string $launchViewParameter JavaScript String, which will be passed as parameters to top.launchView
      * @return string
      */
-    protected function generateReferenceToolTip($referenceCount, $referenceFromCount, $launchViewParameter = '')
+    protected function generateBidirectionalReferenceToolTip($referenceCount, $referenceFromCount, $launchViewParameter = '')
     {
         if (!$referenceCount && !$referenceFromCount) {
             $htmlCode = '-';
